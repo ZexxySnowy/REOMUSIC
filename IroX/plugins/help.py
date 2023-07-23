d6c65af7f05b120cd22d82f5a024db0b551f575f
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-07-23 06:04:48 +0530

    Update help.py

--- IroX/plugins/help.py
+++ IroX/plugins/help.py
@@ -59,7 +59,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
+        await update.reply_sticker("CAACAgUAAxkBAAEDFMZkvHVXiFzz5w7yHJHD76IdRdc_6QACjAoAAlOf4VXd3TmxrqCWHy8E")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
