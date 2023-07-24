c8c665c18b4294de302ffcaa67cf55c0fc7a1859
DJ <119585273+Chetanthakur03@users.noreply.github.com>
2023-07-24 20:23:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ LOG_GROUP_ID = int(getenv("LOG_GROUP_ID",))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "˹ᴘᴧɴᴅᴧ ꭙ ᴍᴜsɪᴄ˼")
 
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6045293810").split())
+    map(int, getenv("OWNER_ID", "5161032951").split())
 )
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
@@ -40,9 +40,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/TheAltron")
+    "SUPPORT_CHANNEL", "https://t.me/PandaMusicSupport")
 SUPPORT_GROUP = getenv(
-    "SUPPORT_GROUP", "https://t.me/AltronChats")
+    "SUPPORT_GROUP", "https://t.me/PandaMusicUpdates")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
